984db0049b7c2b20ca22d6a2de13519b8731d0e5
Nour05Nasr <nour.a.nasr05@gmail.com>
2025-11-27 09:31:55 +0200

    check out

--- src/Components/Common/Button.jsx
+++ src/Components/Common/Button.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 import './Button.css';
 
 const Button = (props) => {
     return ( 
         <>
             <div className="cta_div">
-                <button className="cta_temp_1">{props.title}</button>
+                <Link to={props.url}><Button title={props.title} /></Link>
+                {/* <button className="cta_temp_1">{props.title}</button> */}
                 <div className="cta_bg"></div>
             </div>
         </>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -91,7 +91,7 @@ const techLogos = [
             <div class="div_temp">
                 <H1 title='About Me'/>
                 <P title='Welcome there, you have landed on my creative heaven. I’m a UI/UX Designer, Art Director, Graphic Designer and Front-End Developer. As a UI/UX designer I help to make your ideas come to life into an impactful visual identity, and user-friendly experiences. From branding walking through UI/UX Design to front-end development. I the between creativity and functionality. With experience in branding, UI/UX Design, and digital product design, I make designs that do not only look great but also make a real impact.'/>
-                <Button title='Let’s build your next project'/>
+                <Button title='Know More About Me' url=''/>
             </div>
         </section>
 </section>
